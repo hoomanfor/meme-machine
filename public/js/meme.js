427258c0c139a1d93ecdc39513bb593109292b3d
Hoo Man <hoomanfor@gmail.com>
2019-12-28 13:36:06 -0500

    implemented test canvas where I render text onto image

--- public/js/meme.js
+++ public/js/meme.js
@@ -1,1 +1,16 @@
-console.log("This works!");
+function loadAndDrawImage(src) {
+  // Create an image object. (Not part of the dom)
+  const image = new Image();
+  // After the image has loaded, draw it to the canvas
+  image.onload = () => { 
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
+    ctx.font = "40px Georgia";
+    ctx.fillText("Nothing has to be true.", 0, 100);
+  };
+  image.src = src;
+  console.log("image object", image);
+}
+
+loadAndDrawImage("/images/img-1.jpg");
