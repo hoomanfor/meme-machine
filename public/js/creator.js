9a368fb27470f4a456475cdabd448708a4a472c4
Hoo Man <hoomanfor@gmail.com>
2020-01-02 17:28:29 -0500

    file upload into div background-image

--- public/js/creator.js
+++ public/js/creator.js
@@ -9,4 +9,12 @@ $(".hamburger").on("click", function(event) {
 
 $(":file").on("change", function(event) {
   console.log("This works!");
+  const bgImage = $(this).get(0).files[0];
+  const src = URL.createObjectURL(bgImage);
+  console.log(bgImage);
+  console.log(src);
+  $("#bg-image").attr("src", src);
+  $(".artboard").css("background-image", "url(" + src + ")");
 });
+
+// background-image: url('imagePath');
