4aec2da90c8a53dd3954634f9dd335fe21bc6fc4
Hoo Man <hoomanfor@gmail.com>
2020-01-12 18:07:58 -0500

    allowTaint set to true fixed html2canvas

--- public/js/creator.js
+++ public/js/creator.js
@@ -1,4 +1,3 @@
-
 const firebaseConfig = {
   apiKey: 'AIzaSyA2PDDd2nQlPQwRp4lVpX3BhF8HZs5RNXU',
   authDomain: 'meme-machine-b458b.firebaseapp.com',
@@ -12,8 +11,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const storage = firebase.storage();
-var storageRef = firebase.storage().ref();
-var klaraRef = storageRef.child('klara.txt');
+const storageRef = firebase.storage().ref();
+const klaraRef = storageRef.child('klara.txt');
 
 // var message = 'Klara' // use the Blob or File API
 // klaraRef.putString(message).then(function(snapshot) {
@@ -31,25 +30,38 @@ $('.hamburger').on('click', function (event) {
 
 $(':file').on('change', function (event) {
   console.log('This works!');
-  $('.filename').val(event.target.files[0].name);
-  console.log(event.target.files[0].name.length);
   const bgImage = $(this).get(0).files[0];
+  console.log('bgImage', bgImage);
   const src = URL.createObjectURL(bgImage);
-  console.log(bgImage);
-  console.log(src);
-  $('#bg-image').attr('src', src);
-  $('.artboard').css('background-image', 'url(' + src + ')');
-  // $(this).prop("disabled", true);
+  console.log('src', src);
+  $('#capture').css('background-image', 'url(' + src + ')');
+  // $('.filename').val(event.target.files[0].name);
+  // console.log(event.target.files[0].name.length);
+  // console.log(event.target.files[0].name.length);
+  // const src = URL.createObjectURL(bgImage);
+  // console.log(bgImage);
+  // console.log(src);
+  // $('#bg-image').attr('src', src);
+  // $('#capture').css('background-image', 'url(' + src + ')');
 });
 
 $('#save-meme').on('click', function (event) {
-  html2canvas(document.querySelector(".custom-file-input")).then(canvas => {
-    document.body.appendChild(canvas);
-    let link = $("<a>");
-    link.html("Download!");
-    link.attr("href", canvas.toDataURL());
-    link.attr("download", "test-download.png");
-    $("body").append(link);
+  // html2canvas(document.querySelector('.artboard2')).then(canvas => {
+  //   document.body.appendChild(canvas);
+  //   const link = $('<a>');
+  //   link.html('Download!');
+  //   link.attr('href', canvas.toDataURL());
+  //   link.attr('download', 'test-download.png');
+  //   $('body').append(link);
+  // });
+  html2canvas(document.getElementById('capture'), { scrollX: 0, scrollY: -window.scrollY, allowTaint: true }).then(canvas => {
+    $('body').append(canvas);
   });
+  // html2canvas(document.getElementById('capture')).then(canvas => {
+  //   $('body').append(canvas);
+  // });
 });
 
+
+  // $(this).prop("disabled", true);
+  // $(this).css('display', 'none');
